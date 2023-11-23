ded649b3a7bca48ec5c3d08210bca6d520619e0b
NicCarp11 <nic.carp11@gmail.com>
2023-11-23 11:47:52 +0100

    Implementation of convolution expoliting interleaved bus.

--- sw/applications/kernel_test/main.c
+++ sw/applications/kernel_test/main.c
@@ -44,6 +44,7 @@
 #include "kernels/sha/sha.h"
 #include "kernels/strsearch/strsearch.h"
 #include "kernels/sqrt/sqrt.h"
+#include "kernels/conv_interleaved/conv_interleaved.h"
 
 /****************************************************************************/
 /**                                                                        **/
@@ -76,15 +77,16 @@
 /****************************************************************************/
 
 static kcom_kernel_t *kernels[] = {
-         &conv_kernel,
+        // &conv_kernel,
         // &reve_kernel,
         // &bitc_kernel,
         // &sqrt_kernel,
         // &gsm_kernel,
         // &strs_kernel,
         // &sha_kernel,
-        // &sha2_kernel,
+         //&sha2_kernel,
         // Add all other kernels here
+        &conv_interleaved_kernel
     };
 
 static kcom_perf_t  kperf;
